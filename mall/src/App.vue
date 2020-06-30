bc1facdc70808ba72e8e5eac6486eac16539d5be
yinshenghao <ysh0806@sina.com>
2020-06-30 18:03:38 +0800

    Swiper

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -11,7 +11,7 @@
   export default {
     name: 'App',
     components: {
-      MainTabBar
+      MainTabBar,
     }
   }
 </script>
--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-      <h2>Home</h2>
-    </div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from "../../components/common/navbar/NavBar";
+  import {getHomeMultiData} from "../../network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      // 1、请求多个数据
+      getHomeMultiData().then(res => {
+        console.log(res);
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
